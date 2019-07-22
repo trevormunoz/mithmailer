859346f87f144369397030d5e3e5e3c6e96cb328
Trevor Muñoz <trevor@trevormunoz.com>
2019-07-22 17:52:41 -0400

    Fix errant table padding

--- src/EmailSection.js
+++ src/EmailSection.js
@@ -4,7 +4,6 @@ import { Item, Box } from "react-html-email";
 function EmailSection(props) {
     return (
         <Item align={props.align} valign={props.valign}>
-          <Box border="0" cellpadding="20" cellspacing="0" width={props.width} id={props.idString}>
           <Box border="0" cellpadding="0" cellspacing="0" width={props.width} id={props.idString}>
               {props.children}
           </Box>
