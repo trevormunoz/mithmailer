329f8cc8c90de722f0be6ea25221e5a919882667
Trevor Muñoz <trevor@trevormunoz.com>
2019-07-22 16:49:29 -0400

    Add react-helmet

--- src/Email.js
+++ src/Email.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Helmet } from 'react-helmet';
 import EmailSection from "./EmailSection"
 
 function Email() {
   return (
     <>
+    <Helmet>
+      <title>MITH Turns 20</title>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </Helmet>
     <EmailSection idString="emailHeader" />
     <EmailSection idString="emailBody" />
     <EmailSection idString="emailFooter" />
