676631a0e4f9afde0f8ed2f4fc8a22303a0844fe
Trevor Muñoz <trevor@trevormunoz.com>
2019-07-22 15:58:33 -0400

    Strip out CRA basics and reset for email

---
+++ src/Email.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Item } from "react-html-email";
+
+function Email() {
+  return (
+    <Item align="center" valign="top">
+      I'm going to grow up to be an amazing email!
+    </Item>
+  );
+}
+
+export default Email;
