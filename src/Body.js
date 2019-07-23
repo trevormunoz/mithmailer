5d09adf6ed32c81e37a084262d845bc55d400232
Trevor Muñoz <trevor@trevormunoz.com>
2019-07-23 09:05:11 -0400

    Left-align body text and adjust padding

--- src/Body.js
+++ src/Body.js
@@ -4,15 +4,15 @@ import EmailSection from "./EmailSection"
 import { importMDX } from 'mdx.macro';
 
 const bodyStyle = {
-    padding: '40px 0 30px 0'
+    padding: '40px 30px 30px 30px'
 }
 
 const Content = importMDX.sync('./content/bodyContent.mdx')
 
 function Body(props) {
     return(
-            <Item align="center" valign="top" bgcolor="#ffffff" style={bodyStyle}>
         <EmailSection idString={props.idString}>
+            <Item align="left" valign="top" bgcolor="#ffffff" style={bodyStyle}>
                 <Content />
             </Item>
         </EmailSection>
