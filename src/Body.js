1c7fff8dee96f553e7bb92bb1996268dc6e570ea
Trevor Muñoz <trevor@trevormunoz.com>
2019-07-23 08:21:18 -0400

    Add MDX for email content

--- src/Body.js
+++ src/Body.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { Item } from "react-html-email";
 import EmailSection from "./EmailSection"
+import { importMDX } from 'mdx.macro';
 
 const bodyStyle = {
     padding: '40px 0 30px 0'
 }
 
+const Content = importMDX.sync('./content/bodyContent.mdx')
+
 function Body() {
     return(
         <EmailSection>
             <Item align="center" valign="top" bgcolor="#ffffff" style={bodyStyle}>
-                <p>I'm the body of this amazing email</p>
+                <Content />
             </Item>
         </EmailSection>
     )
