038c2b0125cc17a73c8387b5c1d40958902c564b
Trevor Muñoz <trevor@trevormunoz.com>
2019-07-25 06:00:41 -0400

    Use new image component in header

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { Item } from "react-html-email";
 import EmailSection from "./EmailSection"
+import EmailImage from "./Image";
+import { typeStyles } from './utils/sharedStyles';
 
 const headerStyle = {
+    ...typeStyles,
     padding: '40px 0 30px 0'
 }
 
 function Header(props) {
     return(
         <EmailSection idString={props.idString}>
-            <Item align="center" valign="top" bgcolor="#70bbd9" style={headerStyle}>
-                <p>I'm still the header</p>
+            <Item align="center" valign="top" bgcolor="#42454a" style={headerStyle}>
+                <td>
+                    <EmailImage 
+                        src="" 
+                        alt="Some amazing alt text goes here">
+                    </EmailImage>
+                </td>
             </Item>
         </EmailSection>
     )
