af77856e205a4a93e36c7cb5940a1975dfe800aa
Trevor Muñoz <trevor@trevormunoz.com>
2019-07-28 09:00:23 -0400

    Set header image height and color alt text

--- src/Header.js
+++ src/Header.js
@@ -3,20 +3,22 @@ import { Item } from "react-html-email";
 import EmailSection from "./EmailSection"
 import EmailImage from "./Image";
 import { typeStyles } from './utils/sharedStyles';
+import headerImage from './assets/images/MITH-20th-logo_wht_rgb.png'
 
 const headerStyle = {
     ...typeStyles,
-    padding: '40px 0 30px 0'
+    padding: '40px 0 30px 0',
+    color: '#ffffff'
 }
 
 function Header(props) {
     return(
         <EmailSection idString={props.idString}>
-            <Item align="center" valign="top" bgcolor="#42454a" style={headerStyle}>
+            <Item align="center" valign="top" bgcolor="#424242" style={headerStyle}>
                 <td>
                     <EmailImage 
-                        src="" 
-                        alt="Some amazing alt text goes here">
+                        alt="Some amazing alt text goes here"
+                        height="250">
                     </EmailImage>
                 </td>
             </Item>
